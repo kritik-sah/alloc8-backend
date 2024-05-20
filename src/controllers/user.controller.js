19385a1bfff32ce7dbd284e73358b580022ea1d7
Kritik <kritiksah0@gmail.com>
2024-05-20 22:10:53 +0530

    backup

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,10 +2,11 @@ import User from "../models/user.models.js";
 
 export const getUsers = async (req, res) => {
   try {
-    const logedInUser = req.user._id;
-    const filteredUsers = await User.find({ _id: { $ne: logedInUser } }).select(
-      "-password"
-    );
+    const filteredUsers = await User.find()
+      .select("-password") // Exclude password field
+      .sort({ points: -1 }) // Sort by points in descending order
+      .limit(10); // Limit to 10 results
+
     res.status(200).json(filteredUsers);
   } catch (error) {
     res.status(500).json({ error, message: "error while getting users" });
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,8 @@
 import express from "express";
 import { getUsers } from "../controllers/user.controller.js";
-import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.get("/", protectRoute, getUsers);
+router.get("/", getUsers);
 
 export default router;
